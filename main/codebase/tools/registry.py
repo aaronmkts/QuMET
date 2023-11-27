880ec801ca2aa077b60e688c49db07fcbe61feee
Aaron <aaronmkts@gmail.com>
2023-11-27 16:13:50 +0000

    Update utils folder with cusomt logger and CACHE directory

---
+++ main/codebase/tools/registry.py
@@ -0,0 +1,4 @@
+from pathlib import Path
+
+MAIN_DIR = Path(__file__).resolve().parents[2]
+MAIN_CACHE_DIR = MAIN_DIR / ".main_cache"
--- main/codebase/tools/trainer_utils.py
+++ main/codebase/tools/trainer_utils.py
@@ -3,6 +3,46 @@ from torch.optim.lr_scheduler import LambdaLR
 from functools import partial 
 from typing import Union, Optional
 from torch.optim import Optimizer
+import torch
+from accelerate import Accelerator
+
+def get_optimizer(accelerator:Accelerator,
+                   model: torch.nn.Module, 
+                   optimizer: str, 
+                   learning_rate: float, 
+                   weight_decay: float):
+    no_decay = ["bias", "LayerNorm.weight"]
+    optimizer_grouped_parameters = [
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if not any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": weight_decay,
+        },
+        {
+            "params": [
+                p
+                for n, p in model.named_parameters()
+                if any(nd in n for nd in no_decay)
+            ],
+            "weight_decay": 0.0,
+        },
+    ]
+    match optimizer:
+        case "adamw":
+            optimizer = torch.optim.AdamW(
+                optimizer_grouped_parameters, lr=learning_rate
+            )
+        case "adam":
+            optimizer = torch.optim.Adam(optimizer_grouped_parameters, lr=learning_rate)
+        case "sgd":
+            optimizer = torch.optim.SGD(optimizer_grouped_parameters, lr=learning_rate)
+        case _:
+            raise ValueError(f"Unsupported optimizer: {optimizer}")
+    optimizer = accelerator.prepare(optimizer)
+    return optimizer
 
 class ExplicitEnum(str, Enum):
     """
