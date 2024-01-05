1525a466258c9ec836c1a20e3893f9c60a274306
Harry Youel <harryyouel13@gmail.com>
2024-01-05 18:59:21 +0000

    - Fixed import issues - Redefined get_agent function - Outlined plans for train function

--- main/codebase/actions/rl_train.py
+++ main/codebase/actions/rl_train.py
@@ -1,13 +1,4 @@
 import os
-import sys
-
-os.environ["PYTHONBREAKPOINT"] = "ipdb.set_trace"
-sys.path.append(
-     os.path.join(
-         os.path.dirname(os.path.realpath(__file__)), "..", "..", ".." ,"main","codebase"
-     )
-    )
-
 import gym3
 import gym
 import gymnasium
@@ -16,37 +7,40 @@ from stable_baselines3 import A2C, PPO
 from stable_baselines3.common.evaluation import evaluate_policy
 from gyms.environments import *
 
-def train_agent(agent_type, policy, env, gamma, learning_rate, policy_kwargs, n_epochs=4, clip_range=0.2):
-    print("Start training")
+def get_agent(agent, policy, env, gamma, learning_rate, policy_kwargs, save_path, n_epochs=4, clip_range=0.2):
 
-    if agent_type == "ac2":
-        # Agent
-        a2c_model = A2C(policy,
-                        env,
-                        gamma=gamma,
-                        learning_rate=learning_rate,
-                        policy_kwargs=policy_kwargs,
-                        tensorboard_log='logs/')
+    match agent.lower(): 
+        case "a2c":
+            agent = A2C(policy,
+                            env,
+                            gamma=gamma,
+                            learning_rate=learning_rate,
+                            policy_kwargs=policy_kwargs,
+                            tensorboard_log=save_path)
+            
+        case "ppo":
+            agent = PPO(policy,
+                            env,
+                            gamma=gamma,
+                            n_epochs=n_epochs,
+                            clip_range=clip_range,
+                            learning_rate=learning_rate,
+                            policy_kwargs=policy_kwargs,
+                            tensorboard_log=save_path)
         
-        try:
-            a2c_model.learn(total_timesteps=20000)
-            print("End training for A2C")
-        except Exception as e:
-            print(f"Training for A2C failed: {e}")
+        case _:
+            raise ValueError(f"Unsupported agent: {agent.lower()}")
         
-    elif agent_type == "ppo":
-        # Agent
-        ppo_model = PPO(policy,
-                        env,
-                        gamma=gamma,
-                        n_epochs=n_epochs,
-                        clip_range=clip_range,
-                        learning_rate=learning_rate,
-                        policy_kwargs=policy_kwargs,
-                        tensorboard_log='logs/')
+    return agent
 
-        try:
-            ppo_model.learn(total_timesteps=20000)
-            print("End training for PPO")
-        except Exception as e:
-            print(f"Training for PPO failed: {e}")
+def train(save_path):
+
+    if save_path is not None:
+        # if save_path is None, the model will not be saved
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+    # GPU 
+    # exp_config
+
+    pass
